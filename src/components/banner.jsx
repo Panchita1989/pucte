730686d85baf0cac2142f49a817c022ac28a9984
Panchita <franciscalandwehr@hotmail.com>
2025-10-28 12:15:17 -0500

    added animation

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -2,10 +2,10 @@ import Button from './button.jsx'
 
 export default function Banner(){
     return(
-        <section className='text-neutral-300 mt-40 p-2 flex flex-col justify-center items-center  gap-3'>
-            <h1 className='text-5xl p-2 border-t-2 border-b-2'>PUCTÉ</h1>
-            <p className='text-2xl'>ESCAPE TO PARADISE</p>
-            <Button className='bg-teal-950 p-2 rounded md:hover:cursor-pointer' content='Book now' />
+        <section className='banner text-neutral-300 mt-40 p-2 flex flex-col justify-center items-center gap-3'>
+            <h1 className='h1 text-5xl p-2 h1-animate'>PUCTÉ</h1>
+            <p className='banner-content text-2xl content-animate'>ESCAPE TO PARADISE</p>
+            <Button className='banner-content bg-teal-950 p-2 rounded content-animate md:hover:cursor-pointer' content='Book now' />
         </section>
     )
 }
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@
 
 export default function Footer() {
     return(
-        <section className='flex flex-col md:flex-row gap-5 items-center md:justify-around rounded bg-zinc-300/50 fixed bottom-0 left-0 w-full'>
+        <section className='footer flex flex-col md:flex-row gap-5 items-center md:justify-around rounded bg-zinc-300/50 fixed bottom-0 left-0 w-full'>
             <section className='text-center'>
                 <h2>LOCATION</h2>
                 <p>Carretera Federal 307 KM 23 <br /> Bacalar, Quintana Roo, México</p>
