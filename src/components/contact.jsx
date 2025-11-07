15d57dda40b20700c24976813ba282b5de4d3310
Panchita <franciscalandwehr@hotmail.com>
2025-11-07 12:00:01 -0500

    added contact form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,11 +1,102 @@
-import React from 'react'
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
+import Footer from './footer.jsx'
 
 function Contact() {
+  const [state, handleSubmit] = useForm("mdkynnob");
+  if (state.succeeded) {
+      return <p>Thanks for joining!</p>;
+  }
   return (
-    <div>
-      <h1>Please contact us</h1>
-    </div>
-  )
+    <>
+    <section className='h-screen rounded xl:mx-20 mx-2 my-5 my-1 py-5 px-3 bg-teal-950/80 text-neutral-300 tracking-widest leading-loose transition-opacity duration-700 ease-out opacity-0 animate-[fadeIn_0.8s_ease-out_forwards]'>
+      <h1 className='text-center text-3xl font-bold mb-2'>Contact</h1>
+      <p className='text-center' >For more information please contact us:</p>
+      
+       <form onSubmit={handleSubmit} className='mt-10 flex flex-col justify-start items-center gap-4 w-full max-w-2xl mx-auto'>
+        
+        <fieldset className='w-full'>
+        <div className='flex flex-col md:flex-row gap-4'>
+          <div className='flex flex-col flex-1'>
+            <label htmlFor="firstName" className='mb-1'>First Name</label>
+            <input
+              id="firstName"
+              type="text" 
+              name="firstName"
+              className='border rounded p-2'
+              required
+            />
+          </div>
+          <div className='flex flex-col flex-1'>
+            <label htmlFor="lastName" className='mb-1'>Last Name</label>
+            <input
+              id="lastName"
+              type="text" 
+              name="lastName"
+              className='border rounded p-2'
+            />
+          </div>
+        </div>
+      <ValidationError 
+        prefix="Name" 
+        field="Name"
+        errors={state.errors}
+      />
+      </fieldset>
+      <div className='flex flex-col w-full'>
+        <label htmlFor="email" className='mb-1'>Email Address</label>
+        <input
+          id="email"
+          type="email" 
+          name="email"
+          className='border-1 rounded p-2'
+          required
+        />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      </div>
+      <div className='flex flex-col w-full'>
+        <label htmlFor="subject" className='mb-1'>Subject</label>
+        <input
+          id="subject"
+          type="text" 
+          name="subject"
+          className='border-1 rounded p-2'
+          required
+        />
+      <ValidationError 
+        prefix="Subject" 
+        field="subject"
+        errors={state.errors}
+      />
+      </div>
+      <div className='flex flex-col w-full'>
+        <label htmlFor="message" className='mb-1'>Message</label>
+        <textarea
+          id="message"
+          name="message"
+          rows={5}
+          className='border rounded p-2'
+          required
+        />
+        <ValidationError 
+          prefix="Message" 
+          field="message"
+          errors={state.errors}
+        />
+      </div>
+
+      <button type="submit" disabled={state.submitting} className='bg-teal-950 text-neutral-300 p-2 rounded hover:bg-neutral-300 active:bg-neutral-300 hover:text-teal-950 active:text-teal-950 mt-2 w-full'>
+        Submit
+      </button>
+    </form>
+    </section>
+    <Footer />
+    </>
+  );
 }
 
 export default Contact
