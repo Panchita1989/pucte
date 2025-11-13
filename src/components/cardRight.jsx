1a8d6584e6d00630fe800be347cca4455363781e
Panchita <franciscalandwehr@hotmail.com>
2025-11-13 11:06:28 -0500

    added collapse to room component

--- src/components/cardRight.jsx
+++ src/components/cardRight.jsx
@@ -4,14 +4,14 @@ export default function CardRight({title, children, img, alt, images}){
 
     return(
         <>           
-            <section className='flex flex-col xl:flex-row md:items-center justify-center gap-8'> 
-                <section className='m-5 xl:p-10 xl:w-1/2 flex justify-center'>
+            <section className='flex flex-col xl:flex-row md:items-center justify-center xl:gap-8'> 
+                <section className='xl:m-5 xl:p-10 xl:w-1/2 flex justify-center'>
                     {img ? (
                         <img src={img} alt={alt || title} className='rounded' />
                     ) : <Gallery images={images} />}
                 </section>
                 <section className='m-5 xl:p-10 xl:w-1/2 leading-relaxed text-neutral-300'>
-                <h2 className='text-center mb-5 mt-5 pb:5'>{title}</h2>
+                <h2 className='text-center mb-5 pb:5'>{title}</h2>
                     {children}
                 </section>
             </section>
--- src/components/rooms.jsx
+++ src/components/rooms.jsx
@@ -12,7 +12,15 @@ import { useCollapse } from "react-collapsed";
 
 export default function Rooms() {    
 
-    const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
+    const { 
+        getCollapseProps: getCollapsRooms,
+        getToggleProps: getToggleRooms,
+        isExpanded: isRoomsExpanded
+     } = useCollapse();
+    const { 
+        getCollapseProps: getCollapseSpaces,
+        getToggleProps: getToggleSpaces, 
+        isExpanded: isSpacesExpanded } = useCollapse();
 
     return(
         <>  
@@ -31,32 +39,32 @@ export default function Rooms() {
                 <CardLeft title='ROOMS' images={roomsList} >
                             <p className='mb-5  '>Nestled in the heart of nature, Pucté is a tranquil retreat where timeless
                                 design meets the soul of the Mayan jungle.</p>
-                                {isExpanded ? '' : (
-                                    <button className='border-1 rounded p-2' {...getToggleProps()}>Show More</button>
+                                {isRoomsExpanded ? '' : (
+                                    <button className='border-1 rounded p-2' {...getToggleRooms()}>Show More</button>
                                 )}
-                             <section className='mb-5' {...getCollapseProps()}>
+                             <section className='mb-5' {...getCollapsRooms()}>
                                     <p>We offer four exclusive suites,
                                     each thoughtfully named after the sacred numbers of the ancient Mayan culture, 
                                     inviting you to connect with the heritage and rhythm of this land.
                                     Each suite is equipped with air conditioning to ensure a cool and comfortable 
                                     stay. A dedicated work desk is provided for those who need to stay connected or
                                     inspired, and the private bathroom offers a serene space to refresh and unwind.</p>
-                                    <h2>UPSTAIRS ROOMS</h2>    
+                                    <h2 className='mt-8 mb-2'>UPSTAIRS ROOMS</h2>    
                                     <h3>KA'A | HUN</h3>
-                                    <p className='md:mb-5'>Experience tranquility in our upstairs rooms, each featuring a comfortable
+                                    <p>Experience tranquility in our upstairs rooms, each featuring a comfortable
                                         king-size bed perfect for a restful night’s sleep. Wake up to stunning views of
                                         the lagoon from your private terrace, where you can enjoy a morning coffee or
                                         unwind in the evening. The upstairs location provides a more elevated perspective
                                         of the beautiful surroundings, enhancing your stay with panoramic vistas.</p>
-                                    <h2>DOWNSTAIRS ROOMS</h2>
+                                    <h2 className='mt-8 mb-2'>DOWNSTAIRS ROOMS</h2>
                                     <h3>KAN | OX</h3>
                                     <p>Our downstairs rooms offer the convenience of easy access to the main palapa,
                                         communal area, and kitchen, making it ideal for socializing or enjoying a meal.
                                         Step out onto your private terrace to take in the serene lagoon views, a perfect
                                         spot to relax and soak in the natural beauty of Bacalar.</p>
                             </section> 
-                            {isExpanded ? (
-                             <button className='border-1 rounded p-2' {...getToggleProps()}>Show Less</button>
+                            {isRoomsExpanded ? (
+                             <button className='border-1 rounded p-2' {...getToggleRooms()}>Show Less</button>
                             ) : ''}   
                 </CardLeft>
                 <div className='text-center mb-10'>
@@ -64,19 +72,28 @@ export default function Rooms() {
                 </div>
                 <CardRight title='SPACES' images={spacesList}>
                     <h2>PALAPA</h2>
-                    <p className='md:mb-5'>The palapa area is a welcoming communal space featuring a well-equipped kitchen 
+                    <p className='mb-5'>The palapa area is a welcoming communal space featuring a well-equipped kitchen 
                         with all modern appliances, including a stove, fridge, wine fridge, and coffee 
-                        maker. A dining table for six and comfortable sofas create an ideal setting for 
-                        meals and relaxation. This open-air area offers breathtaking views of the deck 
-                        and lagoon, providing a perfect backdrop for socializing and enjoying the natural
-                        beauty of Bacalar.</p>
-                    <h2>PIER</h2>
-                    <p className='md:mb-5'>Step outside and discover our private dock—your front-row seat to the beauty of 
-                        the lagoon. Whether you're watching the sunset paint the sky, reading a book 
-                        with your feet in the water, sunbathing, or enjoying a refreshing swim, this 
-                        peaceful space invites you to slow down and reconnect. For those seeking a bit 
-                        of adventure, kayaks and paddleboards are available to explore the lagoon at 
-                        your own rhythm, guided only by the sound of nature.</p>               
+                        maker.</p>
+                     {isSpacesExpanded ? '' : (
+                        <button className='border-1 rounded p-2' {...getToggleSpaces()}>Show More</button>
+                    )}
+                    <section className='mb-5' {...getCollapseSpaces()}>
+                                <p>A dining table for six and comfortable sofas create an ideal setting for 
+                                    meals and relaxation. This open-air area offers breathtaking views of the deck 
+                                    and lagoon, providing a perfect backdrop for socializing and enjoying the natural
+                                    beauty of Bacalar.</p>
+                                <h2 className='mt-8'>PIER</h2>
+                                <p className='md:mb-5'>Step outside and discover our private dock—your front-row seat to the beauty of 
+                                    the lagoon. Whether you're watching the sunset paint the sky, reading a book 
+                                    with your feet in the water, sunbathing, or enjoying a refreshing swim, this 
+                                    peaceful space invites you to slow down and reconnect. For those seeking a bit 
+                                    of adventure, kayaks and paddleboards are available to explore the lagoon at 
+                                    your own rhythm, guided only by the sound of nature.</p>         
+                    </section>  
+                     {!isSpacesExpanded ? '' : (
+                        <button className='border-1 rounded p-2' {...getToggleSpaces()}>Show Less</button>
+                    )}    
                 </CardRight>
             </main>
             <Footer />
