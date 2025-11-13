6288eabc497e94b33855016b382e1ca31eccc99f
Panchita <franciscalandwehr@hotmail.com>
2025-11-13 11:47:08 -0500

    added spaces data js

---
+++ src/data/spaces.js
@@ -0,0 +1,11 @@
+
+
+export const spacesList =[{
+    id: 1,
+    url: '/images/palapa.webp',
+    alt: 'lagoon front palapa'
+},{
+    id: 2,
+    url: '/images/dock.webp',
+    alt: 'privat pier'
+}]
