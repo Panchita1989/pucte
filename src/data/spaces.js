3692a3cf8d812ca9d8ee063dbcc0fecdc7fe682f
Panchita <franciscalandwehr@hotmail.com>
2025-12-15 09:54:22 -0500

    added fotos to spaces gallery

--- src/data/spaces.js
+++ src/data/spaces.js
@@ -2,10 +2,82 @@
 
 export const spacesList =[{
     id: 1,
-    url: '/images/palapa.webp',
+    url: '/images/spaces/living.jpg',
     alt: 'lagoon front palapa'
 },{
     id: 2,
-    url: '/images/dock.webp',
+    url: '/images/spaces/dock.webp',
+    alt: 'privat pier'
+},{
+    id: 3,
+    url: '/images/spaces/drone.jpg',
+    alt: 'privat pier'
+},{
+    id: 4,
+    url: '/images/spaces/table.jpg',
+    alt: 'privat pier'
+},{
+    id: 5,
+    url: '/images/spaces/sofa.jpg',
+    alt: 'privat pier'
+},{
+    id: 6,
+    url: '/images/spaces/book.jpg',
+    alt: 'privat pier'
+},{
+    id: 7,
+    url: '/images/spaces/breakfast.jpg',
+    alt: 'privat pier'
+},{
+    id: 8,
+    url: '/images/spaces/casa.jpg',
+    alt: 'privat pier'
+},{
+    id: 9,
+    url: '/images/spaces/cat.jpg',
+    alt: 'privat pier'
+},{
+    id: 10,
+    url: '/images/spaces/cat1.jpg',
+    alt: 'privat pier'
+},{
+    id: 11,
+    url: '/images/spaces/friends.jpg',
+    alt: 'privat pier'
+},{
+    id: 12,
+    url: '/images/spaces/fruits.jpg',
+    alt: 'privat pier'
+},{
+    id: 13,
+    url: '/images/spaces/hammocks.jpg',
+    alt: 'privat pier'
+},{
+    id: 14,
+    url: '/images/spaces/handandfruits.jpg',
+    alt: 'privat pier'
+},{
+    id: 15,
+    url: '/images/spaces/plates.jpg',
+    alt: 'privat pier'
+},{
+    id: 16,
+    url: '/images/spaces/pasillo.jpg',
+    alt: 'privat pier'
+},{
+    id: 17,
+    url: '/images/spaces/pillow.jpg',
+    alt: 'privat pier'
+},{
+    id: 18,
+    url: '/images/spaces/romance.jpg',
+    alt: 'privat pier'
+},{
+    id: 19,
+    url: '/images/spaces/sitdown.jpg',
+    alt: 'privat pier'
+},{
+    id: 20,
+    url: '/images/spaces/steps.jpg',
     alt: 'privat pier'
 }]
