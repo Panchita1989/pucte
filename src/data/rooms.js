7070e76c59a7106c772ec81fa7beb6942b8f7912
Panchita <franciscalandwehr@hotmail.com>
2025-12-16 10:19:17 -0500

    deleted apartment fotos

--- src/data/rooms.js
+++ src/data/rooms.js
@@ -8,54 +8,26 @@ export const roomsList = [{
     alt: 'Pucté Hotel rooms with lagoon view'
 },{
     id: 3,
-    url:'/images/rooms/sala.jpg',
-    alt: 'Pucté Hotel rooms with lagoon view'
-},{
-    id: 4,
-    url:'/images/rooms/work.jpg',
-    alt: 'Pucté Hotel rooms with lagoon view'
-},{
-    id: 5,
-    url:'/images/rooms/casa.jpg',
-    alt: 'Pucté Hotel rooms with lagoon view'
-},{
-    id: 6,
     url:'/images/rooms/chair.jpg',
     alt: 'Pucté Hotel rooms with lagoon view'
 },{
-    id: 7,
-    url:'/images/rooms/corner.jpg',
-    alt: 'Pucté Hotel rooms with lagoon view'
-},{
-    id: 8,
+    id: 4,
     url:'/images/rooms/cozy.jpg',
     alt: 'Pucté Hotel rooms with lagoon view'
 },{
-    id: 9,
+    id: 5,
     url:'/images/rooms/entrance.jpg',
     alt: 'Pucté Hotel rooms with lagoon view'
 },{
-    id: 10,
+    id: 6,
     url:'/images/rooms/hand.jpg',
     alt: 'Pucté Hotel rooms with lagoon view'
 },{
-    id: 11,
-    url:'/images/rooms/sala.jpg',
-    alt: 'Pucté Hotel rooms with lagoon view'
-},{
-    id: 12,
+    id: 7,
     url:'/images/rooms/shadow.jpg',
     alt: 'Pucté Hotel rooms with lagoon view'
 },{
-    id: 14,
+    id: 8,
     url:'/images/rooms/terace.jpg',
-    alt: 'Pucté Hotel rooms with lagoon view'
-},{
-    id: 15,
-    url:'/images/rooms/view.jpg',
-    alt: 'Pucté Hotel rooms with lagoon view'
-},{
-    id: 16,
-    url:'/images/rooms/water.jpg',
     alt: 'Pucté Hotel rooms with lagoon view'
 }]
