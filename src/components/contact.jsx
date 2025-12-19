5691931fcb267101bc62c70bb2cbc2153e9277ba
Panchita <franciscalandwehr@hotmail.com>
2025-12-19 13:37:41 -0500

    added translation to contact page

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -2,26 +2,28 @@ import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
 import Footer from './footer.jsx'
 import NavBar from './navBar.jsx'
+import { useTranslation } from 'react-i18next'
 
 function Contact() {
+  const{ t, i18n } = useTranslation()
   const [state, handleSubmit] = useForm("mdkynnob");
   if (state.succeeded) {
-      return <p className='text-center mt-80'>Thanks for joining!</p>;
+      return <p className='text-center mt-80'>{t('contact.thanks')}</p>;
   }
   return (
     <>
     
     <section className='h-screen rounded xl:mx-10 md:mx-2 mt-20 mb-5 px-3 bg-teal-950/60 text-neutral-300 tracking-widest leading-loose '>
       <NavBar />
-      <h1 className='pt-2 text-center text-3xl font-bold mb-2'>Contact</h1>
-      <p className='text-center' >For more information please contact us:</p>
+      <h1 className='pt-2 text-center text-3xl font-bold mb-2'>{t('contact.title').toUpperCase()}</h1>
+      <p className='text-center' >{t('contact.subtitle')}</p>
       
        <form onSubmit={handleSubmit} className='mx-10 flex flex-col justify-start items-center gap-4 w-full max-w-2xl mx-auto'>
         
         <fieldset className='w-full'>
         <div className='flex flex-col md:flex-row gap-4'>
           <div className='flex flex-col flex-1'>
-            <label htmlFor="firstName" className='mb-1'>First Name</label>
+            <label htmlFor="firstName" className='mb-1'>{t('contact.first')}</label>
             <input
               id="firstName"
               type="text" 
@@ -31,7 +33,7 @@ function Contact() {
             />
           </div>
           <div className='flex flex-col flex-1'>
-            <label htmlFor="lastName" className='mb-1'>Last Name</label>
+            <label htmlFor="lastName" className='mb-1'>{t('contact.last')}</label>
             <input
               id="lastName"
               type="text" 
@@ -47,7 +49,7 @@ function Contact() {
       />
       </fieldset>
       <div className='flex flex-col w-full'>
-        <label htmlFor="email" className='mb-1'>Email Address</label>
+        <label htmlFor="email" className='mb-1'>{t('contact.email')}</label>
         <input
           id="email"
           type="email" 
@@ -62,7 +64,7 @@ function Contact() {
       />
       </div>
       <div className='flex flex-col w-full'>
-        <label htmlFor="subject" className='mb-1'>Subject</label>
+        <label htmlFor="subject" className='mb-1'>{t('contact.subject')}</label>
         <input
           id="subject"
           type="text" 
@@ -77,7 +79,7 @@ function Contact() {
       />
       </div>
       <div className='flex flex-col w-full'>
-        <label htmlFor="message" className='mb-1'>Message</label>
+        <label htmlFor="message" className='mb-1'>{t('contact.message')}</label>
         <textarea
           id="message"
           name="message"
@@ -93,7 +95,7 @@ function Contact() {
       </div>
 
       <button type="submit" disabled={state.submitting} className='bg-teal-950 text-neutral-300 p-2 rounded hover:bg-neutral-300 active:bg-neutral-300 hover:text-teal-950 active:text-teal-950  w-full'>
-        Submit
+        {t('contact.send')}
       </button>
     </form>
     </section>
