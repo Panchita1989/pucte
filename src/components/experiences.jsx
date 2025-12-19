504f821386cb3dfebebdfafd89b9e12e28afd773
Panchita <franciscalandwehr@hotmail.com>
2025-12-19 11:11:31 -0500

    added translation to experiences page

--- src/components/experiences.jsx
+++ src/components/experiences.jsx
@@ -3,10 +3,13 @@ import CardRight from './cardRight.jsx'
 import NavBar from './navBar.jsx'
 import Footer from './footer.jsx'
 import Button from './button.jsx'
+import { useTranslation } from 'react-i18next'
 
 
 
 function Experiences() { 
+
+    const { t, i18n } = useTranslation()
 
       const massage = "Hi 👋 I would like to reserve a massage"
       const urlMassage = `https://api.whatsapp.com/send?phone=529837529659&text=${encodeURIComponent(massage)}`
@@ -15,67 +18,59 @@ function Experiences() {
          <>  
              <section className=' md:mx-10 mt-20 mb-5 py-5 bg-teal-950/60 text-neutral-300 tracking-widest leading-loose'>
                  <NavBar />
-                 <CardLeft title='EXPERIENCES' img='images/sail.png'>
+                 <CardLeft title={t('experiences.title').toUpperCase()} img='images/sail.png'>
                              <p className='pb-5'>
-                              From the dock of Pucté, we sail.
-                              A private escape that departs right from Pucté's dock into Bacalar’s lagoon — just you, the water, 
-                              and the wind. Choose your route: cenotes, mangroves, or secret piers lost in time. Each tour lasts up 
-                              to 3 hours, carried by the breeze.</p>
-
-                              <p className='pb-5'>Private or collective. Sunset or midday. </p>
-                              <h4>Choose your vessel:</h4> 
+                              {t('experiences.description')}
+                              </p>
+                              <p className='pb-5'>{t('experiences.options')}</p>
+                              <h4>{t('experiences.vessel')}</h4> 
                               <ul className='md:mb-5 pb-5'>
                                 <li className='mt-10 flex items-center justify-between border-b pb-2'>
-                                  <h3>Catamaran</h3>
+                                  <h3>{t('experiences.catamaran')}</h3>
                                   <a href="https://sailbacalar.com/" target='_blank'>
                                   <Button 
                                     className='text-[14px] active:bg-teal-950 active:text-neutral-300 xl:hover:bg-teal-950 xl:hover:text-neutral-300 banner-content bg-neutral-300 text-teal-950 p-1 rounded content-animate md:hover:cursor-pointer' 
-                                    content='Book now' />
+                                    content={t('buttons.book')} />
                                     </a></li>
                                 <li className='mt-10 flex items-center justify-between border-b pb-2'>
-                                  <h3>Monohull</h3>
+                                  <h3>{t('experiences.monohull')}</h3>
                                   <a href="" target='_blank'>
                                   <Button 
                                     className='text-[14px] active:bg-teal-950 active:text-neutral-300 xl:hover:bg-teal-950 xl:hover:text-neutral-300 banner-content bg-neutral-300 text-teal-950 p-1 rounded content-animate md:hover:cursor-pointer'
-                                    content='Book now' />
+                                    content={t('buttons.book')} />
                                     </a></li>
                                 <li className='mt-10 flex items-center justify-between border-b pb-2'>
-                                  <h3>Hobby Cat</h3>
+                                  <h3>Hobbie Cat</h3>
                                   <a href="" target='_blank'>
                                   <Button 
                                     className='text-[14px] active:bg-teal-950 active:text-neutral-300 xl:hover:bg-teal-950 xl:hover:text-neutral-300 banner-content bg-neutral-300 text-teal-950 p-1 rounded content-animate md:hover:cursor-pointer'
-                                    content='Book now' />
+                                    content={t('buttons.book')} />
                                   </a></li>
                               </ul>                                            
                  </CardLeft>               
-                 <CardRight title='PADDLE BOARD TOUR' img='images/paddle.png'>
-                     <h6 className='pb-5'>Introductory Stand Up Paddleboard Lesson</h6>
+                 <CardRight title={t('experiences.paddleTitle')} img='images/paddle.png'>
+                     <h6 className='pb-5'>{t('experiences.paddleIntro')}</h6>
                      <p className='md:mb-5 pb-5'>
-                      Start your day with a light breakfast: coffee or tea, seasonal fruit, toast with jam and peanut butter.
-                      Then, enjoy a guided tour to Cenote Negro and Bird Island. (Ask about exclusive routes available for private tours.)
-                      Photos are included to capture the experience.
-                      We provide all the equipment you need: paddleboard, leash, paddle, and life jacket.
-                      <br/>
-                      No prior experience needed—our certified instructors will guide and support you throughout the entire journey.
+                      {t('experiences.paddleDescription')}
                      </p>
                      <div className='text-center mb-10'>
                       <a href="" target='blank'>
                         <Button 
                           className='active:bg-teal-950 active:text-neutral-300 xl:hover:bg-teal-950 xl:hover:text-neutral-300 banner-content bg-neutral-300 text-teal-950 p-2 rounded content-animate md:hover:cursor-pointer' 
-                          content='Book now' />
+                          content={t('buttons.book')} />
                       </a>
                     </div>
                  </CardRight>
                  <CardLeft title='MASSAGE' img='images/massage.webp'>
-                  <p className='pb-5'>Treat yourself to a deeply relaxing massage with the best view Bacalar has to offer — right in front of the
-                    lagoon. You can choose between an individual or couples massage, given by a male or female therapist, either in 
-                    the privacy of your room or directly on the pier, surrounded by the peaceful sounds of nature.</p>
-                  <p>Reserve your Massage now:</p>
+                  <p className='pb-5'>
+                    {t('experiences.massageDescription')}
+                  </p>
+                  
                   <div className='text-center mt-5'>
                       <a href={urlMassage} target='blank'>
                         <Button 
                           className='active:bg-teal-950 active:text-neutral-300 xl:hover:bg-teal-950 xl:hover:text-neutral-300 banner-content bg-neutral-300 text-teal-950 p-2 rounded content-animate md:hover:cursor-pointer' 
-                          content='Book now' />
+                          content={t('buttons.book')}  />
                       </a>
                   </div>
 
